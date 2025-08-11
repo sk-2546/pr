12dd7bf6e7b6bc78637c6dd284831a57fed73867
sk-2546 <deepseek0ai@gmail.com>
2025-08-11 15:42:44 +0530

    Delete project directory

--- project/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for Firebase
-config.resolver.assetExts.push('cjs');
-
-module.exports = config;
