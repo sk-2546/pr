c1cf5e7daa8eb55966067859cae7ecfbe856994a
sk-2546 <deepseek0ai@gmail.com>
2025-09-15 22:24:11 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/voicetype_keyboard/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.voicetype_keyboard.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+}
